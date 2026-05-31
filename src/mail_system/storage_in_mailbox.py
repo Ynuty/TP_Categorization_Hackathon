6b1809845f6ccf4343d029a3b72e753b4152ea6c
dmitrychiyanov06-svg <dmitrychiyanov06@gmail.com>
2026-05-31 18:35:13 +0000

    работа с наименованиями файлов

--- src/mail_system/storage_in_mailbox.py
+++ src/mail_system/storage_in_mailbox.py
@@ -8,3 +8,17 @@ def ensure_mailbox_layout(mailbox_root: Path) -> None:  # создание ст�
     (mailbox_root / "inbox").mkdir(exist_ok=True)
     for category in Category:  # папка для каждой категории из models.py вызывается из main.py перед обработкjq
         (mailbox_root / category.value).mkdir(exist_ok=True)
+
+def unique_dest_path(dest_dir: Path, filename: str) -> Path:  # обработка повторяющихся имен
+    dest = dest_dir / filename
+    if not dest.exists():
+        return dest
+
+    stem = dest.stem  # имя без разрешения
+    suffix = dest.suffix  # добавление расширениня .txt для нового наименования
+    counter = 1
+    while True:
+        candidate = dest_dir / f"{stem}_{counter}{suffix}"
+        if not candidate.exists():
+            return candidate
+        counter += 1
