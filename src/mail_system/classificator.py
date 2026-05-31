9f1c7766bc19f5a848b438b3421b697a0d3c15e9
Ynuty <m.sokolov2047@gmail.com>
2026-05-31 20:05:16 +0300

    Доюавил gitignore

--- src/mail_system/classificator.py
+++ src/mail_system/classificator.py
@@ -1,3 +1,12 @@
 from .models import Category, ClassificationResult, Email
 from .rules import RULES
 
+
+
+class RuleBaseClassificator:
+    def classify(self, email: Email) -> ClassificatorResalt:
+        text = f"{email.subject}\n{email.body}\n{email.sender}"
+
+
+        for rule in RULES:
+            pass
