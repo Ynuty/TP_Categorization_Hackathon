4e9772e973648e285f12475cd0197c3447e90b07
Ynuty <m.sokolov2047@gmail.com>
2026-06-01 19:31:13 +0300

    Добавил первичный logging_info по библиотеке logging

--- src/mail_system/logging_info.py
+++ src/mail_system/logging_info.py
@@ -2,5 +2,24 @@ import logging
 from pathlib import Path
 
 
-def logs_creator(Log_path: Path) -> logging.Logger:
-    pass
+def logs_creator(log_path: Path) -> logging.Logger:
+    log_path.parent.mkdir(parents=True, exist_ok=True) #create папку если еще нет
+
+    logger = logging.getLogger("mail_system") #особенность библиотеки - один логгер для всего
+    logger.setLevel(logging.INFO) # пропускаем не все, только INFO и ERROR (есть еще DEBUG - полная отладка))
+    logger.handlers.clear() # чистим старые обработчики, чтобы логи не повторялись
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    file_handler = logging.FileHandler(log_path, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
